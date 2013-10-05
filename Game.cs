fcf44d8367c5f091836d8ede3cd87e99e9832b91
Sagar <sagar.sahay@gmail.com>
2013-10-05 15:45:42 +0100

    Created a test project

---
+++ Game.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HangMan.Tests
+{
+    public class Game
+    {
+        public int TotalTries { get; set; }
+        public List<char> CorrectGuessedLetters=new List<char>();
+        public List<char> InCorrectGuessedLetters=new List<char>();
+
+        public string MovieName
+        {
+            get;
+            set;
+        }
+
+        public Outcomes PlayerGuessesAWord(char[] movieName)
+        {
+            if (MovieName == string.Join("", movieName) && TotalTries<=10)
+            {
+                return  Outcomes.PlayerWins;
+            }
+            else
+            {
+                if (TotalTries <= 10)
+                {
+                    foreach (char s in movieName)
+                    {
+                        if (MovieName.ToCharArray().Any(x => x == s))
+                        {
+                            if (!CorrectGuessedLetters.Any(y => y == s))
+                            {
+                                CorrectGuessedLetters.Add(s);
+                                TotalTries = TotalTries + 1;
+                            }
+
+                        }
+                        else
+                        {
+                            if (!InCorrectGuessedLetters.Any(y => y == s))
+                            {
+                                InCorrectGuessedLetters.Add(s);
+                                TotalTries = TotalTries + 1;
+                            }
+                        }
+                    }
+                    if()
+                    return Outcomes.PlayerContinues;
+                }
+                else
+                {
+                    return Outcomes.PlayerLooses;
+                }
+                
+            }
+            return Outcomes.PlayerLooses;
+        }
+    }
+}
---
+++ Outcomes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HangMan.Tests
+{
+    public enum  Outcomes
+    {
+        PlayerWins,
+        PlayerContinues,
+        PlayerLooses
+    }
+}
